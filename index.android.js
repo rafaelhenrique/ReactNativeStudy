ab4213b662cd4733364c5a41c8467618e186baf9
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-08-31 16:01:55 -0300

    Working with state

--- index.android.js
+++ index.android.js
@@ -4,16 +4,42 @@ import { AppRegistry, StyleSheet, Text, Image, View } from 'react-native';
 class Greeting extends Component {
   render() {
     return (
-      <Text style={styles.coolstyle}>Hello {this.props.name}!</Text>
+      <Text style={styles.darkgreen}>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {showText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText });
+    }, 1000);
+  }
+
+  render() {
+    // let define var on scope of function
+    // ? is ternary operator
+    let display = this.state.showText ? this.props.dog : ' ';
+    return (
+      <Text style={styles.bigred}>{display}</Text>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  coolstyle: {
+  darkgreen: {
     color: '#006400',
     fontWeight: 'bold',
     fontSize: 30,
+  },
+  bigred: {
+    color: 'red',
+    fontWeight: 'bold',
+    fontSize: 40,
   },
 });
 
@@ -22,10 +48,12 @@ class MyComponent extends Component {
     let pic = {
       uri: 'https://bnetproduct-a.akamaihd.net//dg/7364/334651FFB5510B42E53C9C61CFC413378201C32D.png'
     };
+    // prop can be whatever name, dog for example
     return (
       <View style={{alignItems: 'center'}}>
         <Greeting name="Rafael" />
         <Image source={pic} style={{width: 193, height: 110}}/>
+        <Blink dog='BLINKING! :P' />
       </View>
     );
   }
